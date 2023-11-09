e45aa480c822b948d275e1d6b39cc895ab491feb
Flavia Mabras <flaviamabras@hotmail.com>
2023-11-09 11:42:30 +0100

    exercises 3.13-3.18 solved

--- index.js
+++ index.js
@@ -1,78 +1,83 @@
+require('dotenv').config();
+require('./mongo');
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const Person = require('./models/Person');
+const notFound = require('./middleware/notFound');
+const handleErrors = require('./middleware/handleErrors');
 const app = express();
-
+const Sentry = require('@sentry/node');
+const { ProfilingIntegration } = require('@sentry/profiling-node');
+let info = Person.length;
+const time = new Date();
 app.use(express.static('build'));
 app.use(cors());
 app.use(express.json());
 app.use(
   morgan(':method :url :status :res[content-length] :response-time ms :body')
 );
-
-morgan.token('body', (req, res) => {
+morgan.token('body', (req) => {
   return JSON.stringify(req.body);
 });
 
-let persons = [
-  {
-    id: 1,
-    name: 'Jose Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: ' 12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendick',
-    number: '39-23-6423122',
-  },
-];
-let info = persons.length;
-const time = new Date();
+Sentry.init({
+  dsn: 'https://3dea079ef472a9783998c0a20c5fef53@o4506184287191040.ingest.sentry.io/4506184291188736',
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Sentry.Integrations.Express({ app }),
+    new ProfilingIntegration(),
+  ],
+
+  tracesSampleRate: 1.0,
+
+  profilesSampleRate: 1.0,
+});
 
 app.get('/', (request, response) => {
   response.send('<h1>Phones</h1>');
 });
 app.get('/api/persons', (request, response) => {
-  response.json(persons);
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 app.get('/info', (request, response) => {
   response.send(`<p>Phone has info for ${info} people</p> <br />
   <p>${time}</p>`);
 });
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-  person ? response.send(person) : response.status(404).end();
+app.get('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id;
+  Person.findById(id)
+    .then((person) => {
+      person ? response.send(person) : response.status(404).end();
+    })
+    .catch(next);
 });
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
+app.put('/api/persons/:id', (request, response) => {
+  const id = request.params.id;
+  const person = request.body;
+  const newPersonInfo = {
+    name: person.name,
+    number: person.number,
+  };
+
+  Person.findByIdAndUpdate(id, newPersonInfo, { new: true }).then((result) =>
+    response.status(204).end()
+  );
+});
+app.delete('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id;
+  Person.findByIdAndDelete(id)
+    .then(() => {
+      response.status(204).end();
+    })
+    .catch(next);
   persons = persons.filter((person) => person.id !== id);
   response.status(204).end();
 });
 app.post('/api/persons', (request, response) => {
   const person = request.body;
-  const ids = persons.map((person) => person.id);
-  const generateId = () => {
-    const maxId =
-      persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
-    return maxId + 1;
-  };
-
-  const newPerson = {
-    id: generateId(),
-    name: person.name,
-    number: person.number,
-  };
 
   if (!person.name) {
     return response.status(400).json({
@@ -83,12 +88,19 @@ app.post('/api/persons', (request, response) => {
       error: 'number is empty',
     });
   } else {
-    persons = persons.concat(newPerson);
-    response.status(201).json(newPerson);
+    const newPerson = new Person({
+      name: person.name,
+      number: person.number,
+    });
+    newPerson.save().then((savedPerson) => response.json(savedPerson));
   }
 });
 
-const PORT = process.env.PORT || 3001;
+app.use(notFound);
+app.use(Sentry.Handlers.errorHandler());
+app.use(handleErrors);
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
