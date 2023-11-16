4dbcbb88cc0bc19eb91c4ebee210115806e7e936
Flavia Mabras <flaviamabras@hotmail.com>
2023-11-16 10:12:40 +0100

    delete and move files

--- Part 3/middleware/handleErrors.js
+++
@@ -1,8 +0,0 @@
-module.exports = (error, request, response, next) => {
-  console.log(error);
-  error.name === 'CastError'
-    ? response
-        .status(400)
-        .send({ error: 'the information provided is not correct' })
-    : response.status(500).end();
-};
